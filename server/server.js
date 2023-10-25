f44347600c4f1f8692162956382fdcdc23226e21
Whitney-Rene <whtnchambers76@gmail.com>
2023-10-25 13:31:37 -0400

    installs bcrypt, creates a utils folder, creates function to hash passwords

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 import cors from 'cors';
 
 import { db } from './db/db-connection.js';
+import { hashPassword } from './utils/hashPasswordUtils.js';
 
 config();
 
 const app = express();
 
 const PORT = process.env.PORT || 8888
+
+app.get
 
 app.listen(PORT, () => {
     console.log(`This excellent server is beautifully running on port ${PORT}`);
