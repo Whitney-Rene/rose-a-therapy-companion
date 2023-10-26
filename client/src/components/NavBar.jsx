eb5fe348b4fd4c99288b364eed142a0f7ff038f8
Whitney-Rene <whtnchambers76@gmail.com>
2023-10-26 09:24:47 -0400

    adds a navbar component

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <nav className='navbar'>
+
+        <h1>rose: a therapy companion</h1>
+
+        <div classname="links">
+            <a href="/">Home</a>
+            <a href="/request">request a bouquet</a>
+        </div>
+        
+    </nav>
+  )
+}
