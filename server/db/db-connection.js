68a0634c405dd9f6051ed9238b3e1203ae0567ca
Whitney-Rene <whtnchambers76@gmail.com>
2023-10-25 23:13:21 -0400

    corrects db connection error

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -1,7 +1,10 @@
-import pg from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 
-const db = new pg.Pool ({
+console.log(process.env.DB_URI)
+
+const db = new Pool ({
     connectionString: process.env.DB_URI
-})
+});
 
-export { db }; 
+export default db; 
--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
-import { config } from 'dotenv';
+import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
 
-import { db } from './db/db-connection.js';
+import db from './db/db-connection.js';
 import { hashPassword } from './utils/hashPasswordUtils.js';
-
-// config();
 
 const app = express();
 const PORT = process.env.PORT || 8888
@@ -16,8 +14,39 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json('nuevo comienzo')
-})
+});
+
+// app.get('/users', async (req, res) => {
+//     try {
+//         const { rows: users} = await db.query('SELECT * FROM users');
+//         res.send(users)
+//         // console.log(users);
+//     } catch (error) {
+//         return res.status(400).json({error});
+//     }
+// })
+
+app.get('/users', async (req, res) => {
+    try {
+        const { rows: users } = await db.query('SELECT * FROM users');
+        res.send(users);
+    } catch (error) {
+        console.error('Error in Database Query:', error);
+        return res.status(400).json({ error });
+    }
+});
+
+app.get('/entries', async (req, res) => {
+    try {
+        const { rows: entries } = await db.query('SELECT * FROM entries');
+        res.send(entries);
+    } catch (error) {
+        console.error('Error in Database Query:', error);
+        return res.status(400).json({ error });
+    }
+});
+
 
 app.listen(PORT, () => {
-    console.log(`This excellent server is beautifully running on port ${PORT}`);
+    console.log(`Estoy escuchando en port ${PORT}`);
 });
