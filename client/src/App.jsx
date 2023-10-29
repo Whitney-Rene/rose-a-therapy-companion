fa23bd1b56f9343eb491c2b5f537e3dcfa016759
Whitney-Rene <whtnchambers76@gmail.com>
2023-10-29 16:09:37 -0400

    adds comments to App.jsx and NavBar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,22 @@
-import { useState, useEffect } from 'react';
-//'Switch' has been replaced with 'Routes'
-import { Route, Routes } from 'react-router-dom';
+//imports React's state management and side effect hooks
+import { useState, useEffect } from 'react';  
+//import components from React Router to define routes
+import { Route, Routes } from 'react-router-dom'; //'Switch' has been replaced with 'Routes'
 
+//imports functions and components
 import callBackEnd from '../utils/functions';
-
-import CreateRoseBudThorn from './components/CreateRoseBudThorn';
 import HomePage from './components/HomePage';
 import RequestBouquet from './components/RequestBouquet';
 import NavBar from './components/NavBar';
 import './App.css'
 
-//400 -client side, issue with request
-
 function App() {
 
-
-//fetch users
-
-const [message, setMessage] = useState('');
+//state
+const [message, setMessage] = useState("");
 const [user, setUser] = useState('');
 
+//side effect hook
 useEffect (() => {
 
 //function for async operation, returns a promise
@@ -27,7 +24,10 @@ callBackEnd('')
   //promise based approach/method, called on the promise returned by above function
   //when the promise is resolved (async function is successful), then do the callback function
   //the data - resolved value of the promise, set this as the state
-  .then(data => setMessage(data))
+  .then(data => {
+    setMessage(data);
+    console.log('message:', data)
+  })
   //promise method, used to handle errors
   //if the promise is rejected
   //error represents the error object
@@ -38,7 +38,7 @@ callBackEnd('/users')
   //Curly braces are used to define a code block in arrow functions when you have multiple statements inside the function.
   .then(data => {
     setUser(data);
-    console.log("user data", data);
+    console.log("user data:", data);
   })
   .catch(error => console.error('An error occured:', error));
 
@@ -47,14 +47,22 @@ callBackEnd('/users')
   return (
 
     <div className='App'>
-
+      
+      {/* This component renders logo and "home"/"request bouquet" texts.  
+      The texts hyper links which take users to pages that render the components below. 
+      This NavBar component is outside the Routes commponents, so that user can access the 
+      links, making navigation simple.*/}
       <NavBar />
 
       <div className='content'>
+
+        {/* component of react-router */}
         <Routes>
+          {/* assigns a path to render of these elements */}
           <Route path="/" element={<HomePage message={message}/>} />
           <Route path="/request" element={<RequestBouquet />} />
         </Routes>
+        
       </div>
 
     </div>
@@ -63,3 +71,5 @@ callBackEnd('/users')
 }
 
 export default App
+
+//note: //400 -client side, issue with request
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,31 @@
+//import component to create navigation links
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
+
   return (
+    <>
+   
     <nav className='navbar'>
+
       <p>NavBar Component</p>
       <h1 style={{color: 'sandybrown'}}>rose: a therapy companion</h1>
-        
 
       <div className="links">
-          <Link to="/" style={{color: 'sandybrown', marginRight: '50px'}}>Home</Link>
-          <Link to="/request" style={{color: 'sandybrown'}}>request a bouquet</Link>
+
+        <ul style={{listStyle: 'none'}}>
+
+          {/* when the user clicks "home"/"request about"
+          direct the user to this path, which render an element (comp) */}
+          <li><Link to="/" style={{color: 'sandybrown', marginRight: '50px'}}>Home</Link></li>
+          <li><Link to="/request" style={{color: 'sandybrown'}}>request a bouquet</Link></li>
+          
+        </ul>
+
       </div>
 
     </nav>
+
+    </>
   )
 }
