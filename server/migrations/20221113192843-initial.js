72894f17bf50fcb75af33cebaab2389684d1390b
Whitney-Rene <whtnchambers76@gmail.com>
2023-11-01 13:13:16 -0400

    updates imports

--- server/migrations/20221113192843-initial.js
+++ server/migrations/20221113192843-initial.js
@@ -2,6 +2,9 @@
 
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 var dbm;
 var type;
