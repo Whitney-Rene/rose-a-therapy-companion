13a54ac6e93de16334b015b2a029ac476c0ee401
Whitney-Rene <whtnchambers76@gmail.com>
2023-11-01 23:00:28 -0400

    adds basic form to RequestBouquet.jsx

--- client/src/components/RequestBouquet.jsx
+++ client/src/components/RequestBouquet.jsx
@@ -1,18 +1,31 @@
-
-//import useRef, maybe useState & useEffect too?
-//import function
+import React, { useRef, useState } from 'react';
+import functions from '../../utils/functions';
 
 export default function RequestBouquet() {
 
-  //useRef initialization
+  const userStartDate = useRef(null);
+  const userEndDate = useRef(null);
+  const [bouquetData, setBouquetData] = useState([]);
 
-  //handleSumbit function here
+  
 
   return (
     <div>
       
       <p>RequestBouquet Component</p>
       <p>here you will see an input box for dates to request a bouquet</p>
+
+      <form onSubmit={}>
+        <label>
+          Start Date:
+          <input type="date" ref={userStartDate} />
+        </label>
+        <label>
+          End Date:
+          <input type='date' ref={userEndDate} />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
 
       {/* create a form here, will take 1 or 2 dates - how to make it work with 1 date?
       -onClick function to retrieve and handle data,
