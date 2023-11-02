9eb589f16e00d564122bb24e63d4e2929d1cde53
Whitney-Rene <whtnchambers76@gmail.com>
2023-11-01 21:51:07 -0400

    adds postRequest to handleSubmit function in EntryForm.jsx

--- client/src/components/EntryForm.jsx
+++ client/src/components/EntryForm.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { useParams } from 'react-router-dom';
-// import postRequest from '../../utils/functions';
+import postRequest from '../../utils/functions';
 
 export default function EntryForm() {
 
@@ -20,7 +20,7 @@ export default function EntryForm() {
             entry_content: userEntryContent.current?.value,
         }
 
-        // postRequest("/add-entries/1");
+        postRequest("/add-entries/1", entryData);
         console.log("Form data:", entryData);
 
     }
