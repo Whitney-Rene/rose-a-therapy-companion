4f22a559dc41a0f6e8756897b0cdae2ef66bf403
Whitney-Rene <whtnchambers76@gmail.com>
2023-11-04 12:19:47 -0400

    adds basic setup for login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,21 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Login() {
     return (
         <>
+        <h2>Login</h2>
+            <form>
+                <div>
+                    <Label>Email:</Label>
+                    <input type="email" ref={userEmail} />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type='password' ref={userPassword} />
+                </div>
+                <button onClick={handleLogin}>Log In</button>
+                {loginError && <p>{loginError}</p>}
+            </form>
         </>
     )
 }
