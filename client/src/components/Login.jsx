61118e893c73fe5e501e529550a66f6bf3660383
Whitney-Rene <whtnchambers76@gmail.com>
2023-11-04 12:24:26 -0400

    adds state and refs, begin handleLogin function in Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,16 @@
 import { useState, useRef } from 'react';
 
 export default function Login() {
-    return (
+
+    const [loginError, setLoginError] = useState(null);
+    const userEmail = useRef(null);
+    const userPassword = useRef(null);
+
+    const handleLogin = async () => {
+        
+    };
+    
+        return (
         <>
         <h2>Login</h2>
             <form>
