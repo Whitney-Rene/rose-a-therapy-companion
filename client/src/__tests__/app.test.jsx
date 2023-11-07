534940cc86349760d2fc8104f3e963df8f52f91d
Whitney-Rene <whtnchambers76@gmail.com>
2023-11-07 16:31:32 -0500

    creates test/ and puts app.test.jsx inside it

--- client/src/__tests__/app.test.jsx
+++ client/src/__tests__/app.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from "@testing-library/react";
 // import userEvent from '@testing-library/user-event';
 
-import App from './App';
+import App from '../App';
 
 test ('App renders correctly', () => {
 
