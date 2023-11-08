5293ebac6af12415281c59e0bea64f2589dfb4f8
Whitney-Rene <whtnchambers76@gmail.com>
2023-11-08 11:35:22 -0500

    creates mocks folder and tries to resolve test failure

--- client/src/__tests__/Login.test.jsx
+++ client/src/__tests__/Login.test.jsx
@@ -1,7 +1,25 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from 'react-router-dom';
+import { rest } from 'msw';
+import { setupServer } from "msw/node";
+import { loginHandler } from "../__mocks__/mocks";
+
 
 import Login from "../components/Login";
+
+//creates a server
+const server = setupServer(loginHandler);
+
+//start api testing b/4 tests
+beforeAll(() => {
+  server.listen();
+});
+
+//stop api testing after tests run
+afterAll(() => {
+  server.close();
+});
+
 
 test("Login component functionality", async () => {
     render(
@@ -14,6 +32,11 @@ test("Login component functionality", async () => {
     const passwordInput = screen.getByLabelText("Password:");
     const loginButton = screen.getByText("Login");
 
-    emailInput.value = "beEngWR@gmail.com";
-    passwordInput = ""
-})
+    emailInput.value = 'test@example.com';
+    passwordInput.value = 'password123';
+
+    loginButton.click();
+
+    await screen.findByText('Authentication successful');
+
+});
