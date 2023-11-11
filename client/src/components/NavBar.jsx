3afaf4007dcdaf0aae24c8c69ea723917d8b2393
Whitney-Rene <whtnchambers76@gmail.com>
2023-11-11 10:41:29 -0500

    adds styling to navBar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 //import Link component to create navigation links
 import { Link } from 'react-router-dom';
 import { Typography } from '@mui/material';
+import SpaOutlinedIcon from '@mui/icons-material/SpaOutlined';
 
 import '../css/NavBar.css';
 
@@ -10,21 +11,21 @@ export default function NavBar() {
    
     <div className='navbar'>
 
-          <Typography
-          style={{ color: '#90AACB', fontFamily: 'Patrick Hand, cursive' }}
-          > 
-          rose: a therapy companion 
-          </Typography>
+      <SpaOutlinedIcon className="rose-icon" />
 
-          <div className="links">
-        
-            {/* when the user clicks "Home", "Request a Bouquet" or "Login" links
-            direct the user to this path, which renders an element/comp */}
-            <Link className="nav-link" to="/">home   </Link>
-            <Link className="nav-link" to="/request">request a bouquet   </Link>
-            <Link className="nav-link" to="/login">logout</Link>
+      <Typography className="app-name"> 
+      rose: a therapy companion 
+      </Typography>
 
-          </div>
+      <div className="links">
+    
+        {/* when the user clicks "Home", "Request a Bouquet" or "Login" links
+        direct the user to this path, which renders an element/comp */}
+        <Link className="nav-link" to="/">home</Link>
+        <Link className="nav-link" to="/request">request a bouquet</Link>
+        <Link className="nav-link" to="/login">logout</Link>
+
+      </div>
 
 
     </div>
